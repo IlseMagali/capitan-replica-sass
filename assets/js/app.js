e2b812208ce127d9dfa2c416fd0567ee579f3108
IlseMagali <ilsemke@gmail.com>
2017-06-27 16:56:11 +0000

    arreglando chunches

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+var api = {
+	url:"http://laboratoria.cuadra.co:9339/api/v1/students/"
+};
+
+var cargarPagina = function (){
+	cargarListaAlumnas();
+
+	// console.log("hola");
+};
+
+var cargarListaAlumnas = function () {
+	$.getJSON(api.url, function (alumnas) {
+		alumnas.forEach(crearItemAlumna);
+		// console.log(alumnas);
+	});
+}
+
+var plantilla = "<li>"+"__nombre_alumna__"+"<input type='checkbox'/>"+"</li>";
+
+var crearItemAlumna = function (alumna) {
+	var nombre = alumna.name;
+	var lista =$("#lista-alumnas");
+
+
+	var reemplazarPlantilla = " ";
+	reemplazarPlantilla += plantilla.replace("__nombre_alumna__", nombre);
+
+	console.log(reemplazarPlantilla);
+
+	lista.append(reemplazarPlantilla);
+
+};
+
+$(document).ready(cargarPagina);
